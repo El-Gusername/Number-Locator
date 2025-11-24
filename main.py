2638d55fe461abdf48eac4d7b61e9520d0e5cf13
El-Gusername <elg.username@gmail.com>
2025-11-24 14:25:22 +1300

    Update phone number and reformat code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import phonenumbers
 from phonenumbers import geocoder
 
-number = "+233*********"
+number = "+64210499716"
 
 ch_number = phonenumbers.parse(number, "CH")
 print(geocoder.description_for_number(ch_number, "en"))
@@ -10,3 +10,4 @@ from phonenumbers import carrier
 
 service_number = phonenumbers.parse(number, "RO")
 print(carrier.name_for_number(service_number, "en"))
+
